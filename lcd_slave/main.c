1e1cac048533ad69235b094cbd3cf91da5be2335
TurkeyTitzOnIce <aidenrchristy03@gmail.com>
2025-04-16 14:56:37 -0600

    added almost all lcd functionality, have not tested yet

--- lcd_slave/main.c
+++ lcd_slave/main.c
@@ -8,10 +8,14 @@ int temp = 0;
 char thousands;
 char hundreds;
 char tens;
-char ones;
 int mode;
 int window;
-int time;
+int time_3;
+int time_2;
+int time_1;
+char thousands_2;
+char hundreds_2;
+char tens_2;
 
 
 //---------------LCD Functions-------------------------
@@ -29,33 +33,6 @@ void pulse_enable(){
     P1OUT &= ~BIT0;          // E = 0   
     short_delay();
 }
-
-/*
-void write_nibble(uint8_t data){
-    if(data & BIT0){
-        P1OUT |= BIT4;
-    }else{
-        P1OUT &= ~BIT4;
-    }
-
-    if(data & BIT1){
-        P1OUT |= BIT5;
-    }else{
-        P1OUT &= ~BIT5;
-    }
-
-    if(data & BIT2){
-        P1OUT |= BIT6;
-    }else{
-        P1OUT &= ~BIT6;
-    }
-
-    if(data & BIT3){
-        P1OUT |= BIT7;
-    }else{
-        P1OUT &= ~BIT7;
-    }    
-}*/
 
 void write_nibble(uint8_t data){
     // Clear P1.4–P1.7
@@ -75,34 +52,6 @@ void write_address(uint8_t addy){
     write_nibble(addy_low);
     pulse_enable();
 }
-/*
-void lcd_start(void){
-    __delay_cycles(30000);
-    P1OUT &= ~BIT1;             //Set RS Low
-    write_nibble(0b00000010);
-    pulse_enable();
-    write_nibble(0b00000010);
-    pulse_enable();
-    write_nibble(0b00001100);
-    pulse_enable();
-    __delay_cycles(10);
-    write_nibble(0b00000000);
-    pulse_enable();
-    write_nibble(0b00001111);
-    pulse_enable();
-    __delay_cycles(10);
-    write_nibble(0b00000000);
-    pulse_enable();
-    write_nibble(0b00000001);
-    pulse_enable();
-    __delay_cycles(2000);
-    write_nibble(0b00000000);
-    pulse_enable();
-    write_nibble(0b00000110);
-    pulse_enable();
-    short_delay();
-}
-*/
 
 void lcd_start(void) {
     __delay_cycles(30000); // Wait >15ms after power-on
@@ -134,10 +83,6 @@ void lcd_start(void) {
     __delay_cycles(2000);
     write_address(0x06); // Entry mode set: increment, no shift
 }
-
-
-
-
 
 void write_char(uint8_t data){
     uint8_t high = (data >> 4) & 0x0F;
@@ -203,72 +148,62 @@ void write_mode(void){
 
 
 void write_am(void){
-    uint8_t thousands_up = (thousands & 0xF0) >> 4;
-    uint8_t thousands_low = thousands & 0x0F;
-    uint8_t hundreds_up = (hundreds & 0xF0) >> 4;
-    uint8_t hundreds_low = hundreds & 0x0F;
-    uint8_t tens_up = (tens & 0xF0) >> 4;
-    uint8_t tens_low = tens & 0x0F;
-
     //write temp
     write_address(0x08);
+    long_delay();
 
-    write_nibble(0b00000100);       //Character A
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(0b00000001);
-    pulse_enable();
+    write_char('A');
 
-    write_nibble(0b00000011);       //Character :
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(0b00001010);
-    pulse_enable();
+    write_char(':');
 
-    write_nibble(thousands_up);       //thousands place
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(thousands_low);
-    pulse_enable();
+    write_char(thousands);
 
-    write_nibble(hundreds_up);       //hundreds place
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(hundreds_low);
-    pulse_enable();
+    write_char(hundreds);
 
-    write_nibble(0b00000010);       //Character .
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(0b00001110);
-    pulse_enable();
+    write_char('.');
 
-    write_nibble(tens_up);       //tens place
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(tens_low);
-    pulse_enable();
+    write_char(ten);
 
-    write_nibble(0b00001101);       //Character degree circle thing
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(0b00001111);
-    pulse_enable();
+    write_char(0b11011111);         //Degree circle thing
 
-    write_nibble(0b00000100);       //Character C
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(0b00000011);
-    pulse_enable();
+    write_char('C');
 }
 
 
 void write_window_time(void){
-
-
     write_address(0x40);            //Address 40
+    long_delay();
 
+    write_char(window);
 
+    clear_next();
+
+    write_char(time_3);
+    write_char(time_2);
+    write_char(time_1);
+    write_char('s');
+}
+
+void write_pl(void){
+    //write temp
+    write_address(0x48);
+    long_delay();
+
+    write_char('P');
+
+    write_char(':');
+
+    write_char(thousands_2);
+
+    write_char(hundreds_2);
+
+    write_char('.');
+
+    write_char(ten_2);
+
+    write_char(0b11011111);         //Degree circle thing
+
+    write_char('C');
 }
 
 //----------------------END LCD Functions
@@ -326,15 +261,16 @@ int main(void)
 
 
     while(1) {
-        mode = 2;
-        __delay_cycles(130078);     //Number chosen for 131578 cycles = 1s - 
-                                    //600 cycles worst case senerio for mode write -
-                                    //900 cycles worst case senerio for ambient write
-                                    //= 130078
+        __delay_cycles(119347);     //Number chosen for 131578 cycles = 1s - 
+                                    //1206 cycles worst case senerio for mode write -
+                                    //3809 cycles worst case senerio for ambient write
+                                    //3407 cycles worst case senerio for time window write
+                                    //3809 cycles worst case senerio for plant write
+                                    //= 119347
         write_mode();
-        //write_am();
-        //write_window_time();
-
+        write_am();
+        write_window_time();
+        write_pl();
     }
     
     return 0;
@@ -347,6 +283,13 @@ void record_temp() {
         case 1: hundreds = temp; break;
         case 2: tens = temp; break;
         case 3: mode = temp; break;
+        case 4: window = temp; break;
+        case 5: time_3 = temp; break;
+        case 6: time_2 = temp; break;
+        case 7: time_1 = temp; break;
+        case 8: thousands_2 = temp; break;
+        case 9: hundreds_2 = temp; break;
+        case 10: tens_2 = temp; break;
         default: count = 0; break;
     }
 }
@@ -364,163 +307,3 @@ __interrupt void ISR_TB0_CCR0(void)
 {
     P2OUT ^= BIT0;
 }
-
-/*
-#include "intrinsics.h"
-#include "msp430fr2310.h"
-#include "heartbeat.h"
-#include "stdint.h"
-#include "lcd.h"
-
-uint8_t temp = 0, thousands, hundreds, tens, mode;
-
-const uint8_t MATCH[] = {0x0D, 0x01, 0x04, 0x03, 0x08};
-const uint8_t HEAT[]  = {0x08, 0x05, 0x01, 0x04};
-const uint8_t COOL[]  = {0x03, 0x0F, 0x0F, 0x0C};
-const uint8_t OFF[]   = {0x0F, 0x06, 0x06};
-const uint8_t SET[]   = {0x03, 0x05, 0x04};
-
-void pulse_enable() {
-    P1OUT |= BIT0;
-    __delay_cycles(50);
-    P1OUT &= ~BIT0;
-}
-
-void write_nibble(uint8_t data) {
-    P1OUT = (P1OUT & ~(BIT4 | BIT5 | BIT6 | BIT7)) | ((data & 0x0F) << 4);
-}
-
-void lcd_cmd(uint8_t nib, uint8_t data) {
-    write_nibble(nib);
-    P1OUT &= ~BIT1;
-    pulse_enable();
-    write_nibble(data);
-    pulse_enable();
-}
-
-void lcd_data(uint8_t nib, uint8_t data) {
-    write_nibble(nib);
-    P1OUT |= BIT1;
-    pulse_enable();
-    write_nibble(data);
-    pulse_enable();
-}
-
-void lcd_start() {
-    __delay_cycles(30000);
-    P1OUT &= ~BIT1;
-    write_nibble(0x02); pulse_enable();
-    write_nibble(0x02); pulse_enable();
-    write_nibble(0x0C); pulse_enable();
-    __delay_cycles(10);
-    write_nibble(0x00); pulse_enable();
-    write_nibble(0x0F); pulse_enable();
-    __delay_cycles(10);
-    write_nibble(0x00); pulse_enable();
-    write_nibble(0x01); pulse_enable();
-    __delay_cycles(2000);
-    write_nibble(0x00); pulse_enable();
-    write_nibble(0x06); pulse_enable();
-    __delay_cycles(10);
-}
-
-void clear_next() {
-    lcd_data(0x02, 0x00);
-}
-
-void write_string(const uint8_t* str, uint8_t len) {
-    lcd_cmd(0x08, 0x00);
-    int i;
-    for (i = 0; i < len; i++) {
-        lcd_data(0x04, str[i]);
-    }
-}
-
-void write_mode() {
-    switch (mode) {
-        case 1: write_string(MATCH, 5); break;
-        case 2: write_string(HEAT, 4); clear_next(); break;
-        case 3: write_string(COOL, 4); clear_next(); break;
-        case 4: write_string(OFF, 3); clear_next(); clear_next(); break;
-        case 5: write_string(SET, 3); clear_next(); clear_next(); break;
-    }
-}
-
-void write_am() {
-    uint8_t up, low;
-    lcd_cmd(0x08, 0x08);
-    lcd_data(0x04, 0x01); // A
-    lcd_data(0x03, 0x0A); // :
-
-    up = (thousands & 0xF0) >> 4; low = thousands & 0x0F;
-    lcd_data(up, low);
-
-    up = (hundreds & 0xF0) >> 4; low = hundreds & 0x0F;
-    lcd_data(up, low);
-
-    lcd_data(0x02, 0x0E); // .
-
-    up = (tens & 0xF0) >> 4; low = tens & 0x0F;
-    lcd_data(up, low);
-
-    lcd_data(0x0D, 0x0F); // Degree symbol
-    lcd_data(0x04, 0x03); // C
-}
-
-void record_temp() {
-    static uint8_t count = 0;
-    switch (count++) {
-        case 0: thousands = temp; break;
-        case 1: hundreds = temp; break;
-        case 2: tens = temp; break;
-        case 3: mode = temp; break;
-        default: count = 0; break;
-    }
-}
-
-#pragma vector = EUSCI_B0_VECTOR
-__interrupt void EUSCI_B0_ISR(void) {
-    temp = UCB0RXBUF;
-    record_temp();
-}
-
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void ISR_TB0_CCR0(void) {
-    P2OUT ^= BIT0;
-}
-
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;
-    UCB0CTLW0 = UCSWRST | UCMODE_3;
-    UCB0I2COA0 = 0x0070 | UCOAEN;
-    UCB0CTLW0 &= ~UCMST & ~UCTR;
-    UCB0CTLW0 |= UCSSEL__SMCLK;
-    UCB0BRW = 10;
-    UCB0TBCNT = 0x04;
-    UCB0CTLW1 &= ~(UCASTP_0 | UCASTP_1);
-
-    P1SEL0 |= BIT2 | BIT3;
-    P1SEL1 &= ~(BIT2 | BIT3);
-
-    P2DIR |= BIT0;
-    P2OUT &= ~BIT0;
-
-    UCB0CTLW0 &= ~UCSWRST;
-
-    HeartBeat_init();
-    lcd_init();
-    PM5CTL0 &= ~LOCKLPM5;
-
-    lcd_start();
-    UCB0IE |= UCRXIE0;
-    __enable_interrupt();
-
-    while (1) {
-        __delay_cycles(130078);
-        write_mode();
-        write_am();
-    }
-
-    return 0;
-}
-*/
